951a54408fc21fee6c8e9164b4eab55a16c2c11a
Destin mulendelwa <143407296+mulendelwa@users.noreply.github.com>
2024-03-03 15:59:04 +0200

    Add files via upload
    cette est connecter sur deux base de donnee sql server  et mysql

---
+++ PROJET1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.postnom = new System.Windows.Forms.TextBox();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.adresse = new System.Windows.Forms.TextBox();
+            this.matricule = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.postnom);
+            this.panel1.Controls.Add(this.nom);
+            this.panel1.Controls.Add(this.adresse);
+            this.panel1.Controls.Add(this.matricule);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(779, 368);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(275, 143);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(210, 23);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Adresse de l\'etudiant";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(287, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(179, 23);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Nom de l\'etudiant";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(540, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Post-Nom de l\'etudiant";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 23);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "matricule";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(328, 31);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(237, 225);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(342, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // postnom
+            // 
+            this.postnom.Location = new System.Drawing.Point(536, 100);
+            this.postnom.Name = "postnom";
+            this.postnom.Size = new System.Drawing.Size(231, 20);
+            this.postnom.TabIndex = 4;
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(267, 100);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(235, 20);
+            this.nom.TabIndex = 3;
+            // 
+            // adresse
+            // 
+            this.adresse.Location = new System.Drawing.Point(97, 169);
+            this.adresse.Name = "adresse";
+            this.adresse.Size = new System.Drawing.Size(572, 20);
+            this.adresse.TabIndex = 2;
+            // 
+            // matricule
+            // 
+            this.matricule.Location = new System.Drawing.Point(33, 100);
+            this.matricule.Name = "matricule";
+            this.matricule.Size = new System.Drawing.Size(186, 20);
+            this.matricule.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(779, 48);
+            this.panel2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(779, 368);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox postnom;
+        private System.Windows.Forms.TextBox nom;
+        private System.Windows.Forms.TextBox adresse;
+        private System.Windows.Forms.TextBox matricule;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PROJET1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+using System.Data.Odbc;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection sqlConnection;
+        MySqlConnection mySqlConnection;
+        OdbcConnection progressConnection;
+        string selectedConnection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.Add("SQL Server");
+            comboBox1.Items.Add("MySQL");
+            comboBox1.Items.Add("Progress");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Remplir la ComboBox avec les options de connexion
+            
+        }
+
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String noms = nom.Text;
+            String postnoms = postnom.Text;
+            String matricules = matricule.Text;
+            String adresss = adresse.Text;
+
+
+
+
+            string connectionString = "";
+            switch (selectedConnection)
+            {
+                case "SQL Server":
+                    connectionString = @"Data Source=DESTIN\DESTIN;Initial Catalog=etudi;Integrated Security=True";
+                    sqlConnection = new SqlConnection(connectionString);
+                    sqlConnection.Open();
+
+                    // Insertion des informations des étudiants
+                    SqlCommand cmd = new SqlCommand("INSERT INTO etudi (nom, postnom, matricule, adresse) VALUES (@noms, @postnoms, @matricules, @adress )", sqlConnection);
+                    cmd.Parameters.AddWithValue("@noms", noms);
+                    cmd.Parameters.AddWithValue("@postnoms", postnoms);
+                    cmd.Parameters.AddWithValue("@matricules", matricules);
+                    cmd.Parameters.AddWithValue("@adress", adresss);
+                    cmd.ExecuteNonQuery();
+
+                    sqlConnection.Close();
+                    MessageBox.Show("est inserer avec succes Sur SQL server");
+                    break;
+                case "MySQL":
+                    connectionString = "server=localhost;user id=root;password=  ;database= etudi";
+                    mySqlConnection = new MySqlConnection(connectionString);
+                    mySqlConnection.Open();
+
+                    // Insertion des informations des étudiants
+                    MySqlCommand myCmd = new MySqlCommand("INSERT INTO etudi (nom, postnom, matricule, adresse) VALUES (@noms, @postnoms, @matricules, @adress )", mySqlConnection);
+                    myCmd.Parameters.AddWithValue("@noms", noms);
+                    myCmd.Parameters.AddWithValue("@postnoms", postnoms);
+                    myCmd.Parameters.AddWithValue("@matricules", matricules);
+                    myCmd.Parameters.AddWithValue("@adress", adresss);
+                    myCmd.ExecuteNonQuery();
+
+                    mySqlConnection.Close();
+                    MessageBox.Show("est inserer avec succes Sur MySQL");
+                    break;
+                case "Progress":
+                    connectionString = "Driver={Progress OpenEdge 11.7 Driver};Dsn=DSNName;Uid=UserName;Pwd=Password;";
+                    progressConnection = new OdbcConnection(connectionString);
+                    progressConnection.Open();
+
+                    // Insertion des informations des étudiants
+                    OdbcCommand progressCmd = new OdbcCommand("INSERT INTO Etudiants (Nom, Prenom) VALUES ('John', 'Doe')", progressConnection);
+                    progressCmd.ExecuteNonQuery();
+
+                    progressConnection.Close();
+                    break;
+                default:
+                    MessageBox.Show("Veuillez sélectionner une base de données");
+                    break;
+            }
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedConnection = comboBox1.SelectedItem.ToString();
+        }
+    }
+}
